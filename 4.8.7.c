7675cb58f5ed020ba0c558a5530eef28edda828c
jinjidexinergou <xinyan7788@live.com>
2019-03-24 23:09:07 +0800

    编程练习

---
+++ 4.8.7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<float.h>
+
+int main()
+{
+    double d1=1.0/3.0;
+    float f1=1.0/3.0;
+    printf("%.6f,%.6f\n",d1,f1);
+    printf("%.12f,%.12f\n",d1,f1);
+    printf("%.16f,%.16f\n",d1,f1);
+    printf("%f,%f\n",FLT_DIG,DBL_DIG);
+    return 0;
+}
