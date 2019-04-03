6486bf374bbb22b69066d1838b3e1a6bd9fe0ff8
ivan <41556180+jinjidexinergou@users.noreply.github.com>
2019-04-03 23:27:44 +0800

    Update listing 5.8.c

--- CH05/listing 5.8.c
+++ CH05/listing 5.8.c
@@ -4,7 +4,7 @@
 int main()
 {
     int n=0;
-    typedef unsigned int size_t;
+    typedef unsigned int size_t;//编译器不同根据实际情况是否添加
     size_t intsize;
     intsize=sizeof(int);
     printf("n=%d,n has %u bytes;all ints have %u bytes.\n",n,sizeof n,intsize);
