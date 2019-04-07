dff274086e15ee37ce9d1a9f0acdd1875c982125
jinjidexinergou <xinyan7788@live.com>
2019-04-07 17:32:55 +0800

    递增运算

---
+++ CH05/listing 5.10.c
@@ -0,0 +1,16 @@
+//add_one.c 递增：前缀与后缀
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int ultra=0,super=0;
+    while(super<5)
+    {
+        super++;
+        ++ultra;
+        printf("super=%d,ultra=%d\n",super,ultra);
+
+    }
+    return 0;
+}
