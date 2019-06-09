1e575cc8e5111f1ac168e46db2b9ad0fc1ce0acc
jinjidexinergou <xinyan7788@live.com>
2019-06-09 12:20:06 +0800

    list6.1

--- CH06/list/summing.c
+++ CH06/list/summing.c
@@ -7,12 +7,12 @@ int main()
 
     printf("please enter a integer to be summed ");
     printf("(q to quite): ");
-    status = scanf("%ld", &num);
-    while(status==1)//==为等于
+
+    while(scanf("%ld",&num)==1)//==为等于
     {
         sum = sum + num;
         printf("please enter next integer (q to quite): ");
-        status = scanf("%ld", &num);
+
     }
     printf("those integers sum to %ld.\n",sum);
 
