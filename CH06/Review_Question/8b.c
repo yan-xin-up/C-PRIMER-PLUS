bc19e6e332f2eba6f5a0175d1b01cad9d6f8f550
jinjidexinergou <xinyan7788@live.com>
2019-07-09 23:17:19 +0800

    8b

---
+++ CH06/Review_Question/8b.c
@@ -0,0 +1,17 @@
+//8b---8b.c
+#include <stdio.h>
+int main(void)
+{
+    char ch;
+
+    scanf("%c", &ch);
+
+    while(ch!='g')
+    {
+        printf("%c",++ch);
+        scanf("%c", &ch);
+    }
+
+    return 0;
+}
+
