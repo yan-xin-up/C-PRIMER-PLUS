eb51c93c4055091ff0d531ba79cb095ec5fe871f
jinjidexinergou <xinyan7788@live.com>
2019-07-14 23:50:15 +0800

    14

--- CH06/Review_Question/14.c
+++ CH06/Review_Question/14.c
@@ -1,4 +1,10 @@
-//
-// Created by 辛岩 on 2019-07-14.
-//
+//14.c --- K output
+#include <stdio.h>
+int main(void)
+{
+    int k;
+    for(k = 1,printf("%d\n",k);printf("k = %d\n",k),k*k<26;k+=2,printf("NOW k is %d.\n",k))
+        printf("k is %d in the loop \n",k);
+    return 0;
+}
 
