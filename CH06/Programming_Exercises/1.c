7841f5560af9c8f5fd5562be96a6a95c8b13f3e5
jinjidexinergou <xinyan7788@live.com>
2019-07-15 23:37:48 +0800

    1.c

---
+++ CH06/Programming_Exercises/1.c
@@ -0,0 +1,14 @@
+//zimu--1.c
+#include <stdio.h>
+#define NUMBER 'z'
+
+int main ()
+{
+    char ch [26];
+    int index;
+
+   for (index='a';index<=NUMBER;index+=1)
+       printf("%c",index);
+   return 0;
+}
+
---
+++ CH06/Programming_Exercises/cmake-build-debug/CMakeFiles/Programming_Exercises.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Programming_Exercises.dir/1.c.o"
+  "Programming_Exercises.pdb"
+  "Programming_Exercises"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Programming_Exercises.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
