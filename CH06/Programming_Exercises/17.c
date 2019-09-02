cfaf6e8f0044cd98664057ccf48c169d6a92754f
jinjidexinergou <xinyan7788@live.com>
2019-09-02 23:03:24 +0800

    17

--- CH06/Programming_Exercises/17.c
+++ CH06/Programming_Exercises/17.c
@@ -1,4 +1,15 @@
-//
-// Created by 辛岩 on 2019/9/2.
-//
+//17.c--取钱
+#include <stdio.h>
+int main()
+{
+    double ch = 100;
+    int i = 0;
+    do{
+        i++;
+        ch += ch*0.08;
+        ch = ch-10;
+    }while(ch>0);
+    printf("you need %d",i);
+    return 0;
+}
 
