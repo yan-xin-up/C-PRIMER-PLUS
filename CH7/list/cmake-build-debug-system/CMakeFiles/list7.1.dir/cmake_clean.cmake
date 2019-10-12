c734da921b68c10fb60f634b19547ba499d1a3fd
jinjidexinergou <xinyan7788@live.com>
2019-10-12 23:52:17 +0800

    list

---
+++ CH7/list/cmake-build-debug-system/CMakeFiles/list7.1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/list7.1.dir/colddays.c.o"
+  "CMakeFiles/list7.1.dir/cypher2.c.o"
+  "list7.1"
+  "list7.1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/list7.1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CH7/list/cmake-build-debug-system/CMakeFiles/list7.3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/list7.3.dir/cypher2.c.o"
+  "list7.3"
+  "list7.3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/list7.3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
